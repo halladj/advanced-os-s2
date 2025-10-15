c9e84692987fb46835b0841a19d149e29e5ee363
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-15 22:59:59 +0100

    created main.go

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//TODO: Call the function SayHello, once without the 'go'
+	//      keyword, and another time with the 'go'keyword.
+}
+
+// declare the function SayHello
