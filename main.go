0a76b06db7902c921dcaa651d807cca5d26e2d11
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-15 23:22:30 +0100

    problem

--- main.go
+++ main.go
@@ -1,28 +1,53 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
-// TODO: Define the variable turn.
-// TODO: Define the "flag [2]int " array.
+const N = 3 // number of processes
 
-func Task1() {
+var choosing [N]bool
+var number [N]int
+
+// helper function to get the maximum ticket number
+// Do Not modify, meant to just be used.
+func maxNumber() int {
+	max := 0
+	for i := 0; i < N; i++ {
+		if number[i] > max {
+			max = number[i]
+		}
+	}
+	return max
+}
+
+func Task(id int) {
 	for {
-		//TODO: Complete the code.
+		fmt.Printf("Task-%d: Begin Section\n", id)
 
-		time.Sleep(time.Second * 2)
+		// Entry Section
+		// TODO: 1. Set choosing[id]
+		// TODO: 2. Assign number[id]
+		// TODO: 3. Set choosing[id]
+
+		// TODO: 4. Wait for all other processes (follow Bakery algorithm conditions)
+
+		// Critical Section
+		fmt.Printf(">>> Task-%d: Critical Section <<<\n", id)
+		time.Sleep(time.Second)
+
+		// Exit Section
+		// TODO: 5. Set number[id]
+
+		fmt.Printf("Task-%d: Remainder Section\n", id)
+		time.Sleep(2 * time.Second)
 	}
 }
-func Task2() {
-	for {
-		//TODO: Complete the code.
 
-		time.Sleep(time.Second * 2)
-	}
-}
 func main() {
-	go Task1()
-	go Task2()
-
-	for {
+	for i := 0; i < N; i++ {
+		go Task(i)
 	}
+	select {} // keeps the program running
 }
