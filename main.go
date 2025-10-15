00932c10c15a4a16538e013f9c66054576f1e2a6
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-15 23:04:40 +0100

    critical-section code

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+//TODO: declare the shared variable x.
+
+// TODO: define the increament() function.
+//	     increaments x by 1 + print the current x value.
+
+func main() {
+
+	// TODO: call increament() multiple times, with/wihtout
+	//       'go'keyword.
+
+	for {
+	}
+}
