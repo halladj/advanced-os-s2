9966d741db3e56d36a459851a28062ad9456bdca
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-23 11:05:07 +0100

    part-1

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 // TODO: declare the function Say(word string). It print the
+//
 //	the 'word' three time.
+func Say(word string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(word)
+	}
+}
 
 func main() {
 	//TODO: Call the function Say, with param "hello" using go keyword
+	go Say("Hello")
 
 	//TODO: Call the function Say, with param "world" without go keyword
+	Say("World")
+
+	time.Sleep(time.Second * 2)
+
 }
