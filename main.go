d000d2fb15b48f4c6d010cf824e5e569237ff951
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-23 11:45:21 +0100

    part-3

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // TODO: Define the "flag [2]int " array.
+var flag [2]int
 
 func Task1() {
 	for {
 		//TODO: Complete the code.
+		fmt.Println("---------Begin Section----------")
+		flag[0] = 1
+		for flag[1] == 1 {
+		}
+		fmt.Println("Critical Section")
+		flag[0] = 0
+		fmt.Println("---------Reminding Section----------")
 
 		time.Sleep(time.Second * 2)
 	}
@@ -14,6 +25,13 @@ func Task1() {
 func Task2() {
 	for {
 		//TODO: Complete the code.
+		fmt.Println("---------Begin Section----------")
+		flag[1] = 1
+		for flag[0] == 1 {
+		}
+		fmt.Println("Critical Section")
+		flag[1] = 0
+		fmt.Println("---------Reminding Section----------")
 
 		time.Sleep(time.Second * 2)
 	}
