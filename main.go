968bf3a18c891791a9a880315c211c093f883f99
Hamza Halladj <hamza.halladj@univ-constantine2.dz>
2025-10-23 11:18:10 +0100

    part-2

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
-//TODO: declare the shared variable x.
+import "fmt"
+
+// TODO: declare the shared variable x.
+var x int
 
 // TODO: define the increament() function.
-//	     increaments x by 1 + print the current x value.
+//
+//	increaments x by 1 + print the current x value.
+func increment() {
+	x++
+	fmt.Println(x)
+}
 
 func main() {
 
 	// TODO: call increament() multiple times, with/wihtout
 	//       'go'keyword.
+	for i := 0; i < 6; i++ {
+		go increment()
+		increment()
+	}
 
 	for {
 	}
